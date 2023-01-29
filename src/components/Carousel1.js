19421d522a8797b82873b66b7a12168336dbaa03
SamiEroglu <samie.github@gmail.com>
2023-01-29 18:26:48 +0300

    videocarousel

--- src/components/Carousel1.js
+++ src/components/Carousel1.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Carousel from "better-react-carousel";
-
 function Carousel1() {
   return (
     <Carousel cols={1} rows={1} gap={5} loop>
-      <Carousel.Item></Carousel.Item>
+      <Carousel.Item>
+        <video width="85%" controls autoPlay type="video/mp4">
+          <source src="videos/yildiz.mp4" />
+        </video>
+      </Carousel.Item>
       <Carousel.Item></Carousel.Item>
       <Carousel.Item></Carousel.Item>
       <Carousel.Item></Carousel.Item>
