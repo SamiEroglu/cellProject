da259e10a0b940dc33cd5374af1730ffbb63dd12
Melih Karakaya <46146749+Thynic@users.noreply.github.com>
2023-01-29 18:25:46 +0300

    animal cell hover h1s
    animalcell.js > title and explanation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import "./App.css";
-import Carousel1 from "./components/Carousel1";
-import Firstpage from "./components/Firstpage";
-import Sidemenu from "./components/Sidemenu";
+import './App.css';
+import Carousel1 from './components/Carousel1';
+import Firstpage from './components/Firstpage';
+import Sidemenu from './components/Sidemenu';
 
 function App() {
-  return (
-    <div className="App">
-      <body>
-        <div class="bg-animation">
-          <div id="stars"></div>
-          <div id="stars2"></div>
-          <div id="stars3"></div>
-          <div id="stars4"></div>
-        </div>
-      </body>
-      <Firstpage />
-      <Carousel1 />
-      <Sidemenu />
-    </div>
-  );
+	return (
+		<div className="App">
+			<body>
+				<div class="bg-animation">
+					<div id="stars"></div>
+					<div id="stars2"></div>
+					<div id="stars3"></div>
+					<div id="stars4"></div>
+				</div>
+			</body>
+			<Firstpage />
+			<Carousel1 />
+			<Sidemenu />
+		</div>
+	);
 }
 
 export default App;
