5f80d544a950a34d1b4f31acc9f8ed5927a016db
Melih Karakaya <46146749+Thynic@users.noreply.github.com>
2023-03-13 15:17:41 +0300

    videoların daha basit olanlarla değiştirilmesi

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import React, { useState } from "react";
-import Stars from "./components/Stars";
-import Carousel1 from "./components/Carousel1";
-import Carousel2 from "./components/Carousel2";
-import CarouselDifferences from "./components/CarouselDifferences";
-import Differences from "./components/Differences";
-import Firstpage from "./components/Firstpage";
-import Sidemenu from "./components/Sidemenu";
-import { SemipolarSpinner } from "react-epic-spinners";
-import "./App.css";
+import React, { useState } from 'react';
+import Stars from './components/Stars';
+import Carousel1 from './components/Carousel1';
+import Carousel2 from './components/Carousel2';
+import CarouselDifferences from './components/CarouselDifferences';
+import Differences from './components/Differences';
+import Firstpage from './components/Firstpage';
+import Sidemenu from './components/Sidemenu';
+import { SemipolarSpinner } from 'react-epic-spinners';
+import './App.css';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  setTimeout(() => {
-    setIsLoading(false);
-  }, 2250);
-  return (
-    <div className="App">
-      {isLoading ? (
-        <div className="loadingcont">
-          <SemipolarSpinner
-            className="spinner"
-            animationDelay="0"
-            size="400"
-            color="#E0E0E0"
-          />
-          <div className="ellipseshadow"></div>
-        </div>
-      ) : (
-        <div>
-          <Stars />
-          <Firstpage />
-          <Differences />
-          <Carousel1 />
-          <Carousel2 />
-          <CarouselDifferences />
-          <Sidemenu />
-        </div>
-      )}
-    </div>
-  );
+	const [isLoading, setIsLoading] = useState(true);
+	setTimeout(() => {
+		setIsLoading(false);
+	}, 1750);
+	return (
+		<div className="App">
+			{isLoading ? (
+				<div className="loadingcont">
+					<SemipolarSpinner
+						className="spinner"
+						animationDelay="0"
+						size="400"
+						color="#E0E0E0"
+					/>
+					<div className="ellipseshadow"></div>
+				</div>
+			) : (
+				<div>
+					<Stars />
+					<Firstpage />
+					<Differences />
+					<Carousel1 />
+					<Carousel2 />
+					<CarouselDifferences />
+					<Sidemenu />
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/components/Carousel1.js
+++ src/components/Carousel1.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Carousel from "better-react-carousel";
-import "../styles/carousel.css";
+import React from 'react';
+import Carousel from 'better-react-carousel';
+import '../styles/carousel.css';
 function Carousel1() {
 	return (
 		<div className="maincarousel-container">
@@ -12,15 +12,15 @@ function Carousel1() {
 						cols={1}
 						rows={1}
 						gap={20}
-						dotColorActive={"aquamarine"}
-						dotColorInactive={"grey"}
+						dotColorActive={'aquamarine'}
+						dotColorInactive={'grey'}
 						loop
 						mobileBreakpoint
 						showDots
 						scrollSnap
 						responsiveLayout={[
 							{
-								height: "40vw",
+								height: '40vw',
 								breakpoint: 0,
 								cols: 1,
 								rows: 1,
@@ -32,130 +32,104 @@ function Carousel1() {
 					>
 						<Carousel.Item>
 							<video
+								muted
 								width="72%"
 								controls
 								autoPlay
 								type="video/mp4"
 								style={{
-									borderTopRightRadius: "48px",
-									borderTopLeftRadius: "48px",
-									borderBottomRightRadius: "20px",
-									borderBottomLeftRadius: "20px",
+									borderTopRightRadius: '48px',
+									borderTopLeftRadius: '48px',
+									borderBottomRightRadius: '20px',
+									borderBottomLeftRadius: '20px',
 								}}
 							>
-								<source src="videos/yildiz.mp4" />
+								<source src="videos/mitoz1.mp4" />
 							</video>
 						</Carousel.Item>
 						<Carousel.Item>
 							<video
+								muted
 								width="72%"
 								controls
 								autoPlay
 								type="video/mp4"
 								style={{
-									borderTopRightRadius: "48px",
-									borderTopLeftRadius: "48px",
-									borderBottomRightRadius: "20px",
-									borderBottomLeftRadius: "20px",
+									borderTopRightRadius: '48px',
+									borderTopLeftRadius: '48px',
+									borderBottomRightRadius: '20px',
+									borderBottomLeftRadius: '20px',
 								}}
 							>
-								<source src="videos/mitozasama.mp4" />
+								<source src="videos/mitoz2.mp4" />
 							</video>
 						</Carousel.Item>
 						<Carousel.Item>
 							<video
+								muted
 								width="72%"
 								controls
 								autoPlay
 								type="video/mp4"
 								style={{
-									borderTopRightRadius: "48px",
-									borderTopLeftRadius: "48px",
-									borderBottomRightRadius: "20px",
-									borderBottomLeftRadius: "20px",
+									borderTopRightRadius: '48px',
+									borderTopLeftRadius: '48px',
+									borderBottomRightRadius: '20px',
+									borderBottomLeftRadius: '20px',
 								}}
 							>
-								<source src="videos/interfaz1.mp4" />
+								<source src="videos/mitoz3.mp4" />
 							</video>
 						</Carousel.Item>
 						<Carousel.Item>
 							<video
+								muted
 								width="72%"
 								controls
 								autoPlay
 								type="video/mp4"
 								style={{
-									borderTopRightRadius: "48px",
-									borderTopLeftRadius: "48px",
-									borderBottomRightRadius: "20px",
-									borderBottomLeftRadius: "20px",
+									borderTopRightRadius: '48px',
+									borderTopLeftRadius: '48px',
+									borderBottomRightRadius: '20px',
+									borderBottomLeftRadius: '20px',
 								}}
 							>
-								<source src="videos/profaz2.mp4" />
+								<source src="videos/mitoz4.mp4" />
 							</video>
 						</Carousel.Item>
 						<Carousel.Item>
 							<video
+								muted
 								width="72%"
 								controls
 								autoPlay
 								type="video/mp4"
 								style={{
-									borderTopRightRadius: "48px",
-									borderTopLeftRadius: "48px",
-									borderBottomRightRadius: "20px",
-									borderBottomLeftRadius: "20px",
+									borderTopRightRadius: '48px',
+									borderTopLeftRadius: '48px',
+									borderBottomRightRadius: '20px',
+									borderBottomLeftRadius: '20px',
 								}}
 							>
-								<source src="videos/metafaz3.mp4" />
+								<source src="videos/mitoz5.mp4" />
 							</video>
 						</Carousel.Item>
 						<Carousel.Item>
 							<video
+								muted
 								width="72%"
 								controls
 								autoPlay
 								type="video/mp4"
 								style={{
-									borderTopRightRadius: "48px",
-									borderTopLeftRadius: "48px",
-									borderBottomRightRadius: "20px",
-									borderBottomLeftRadius: "20px",
+									borderTopRightRadius: '48px',
+									borderTopLeftRadius: '48px',
+									borderBottomRightRadius: '20px',
+									borderBottomLeftRadius: '20px',
 								}}
 							>
-								<source src="videos/anafaz4.mp4" />
-							</video>
-						</Carousel.Item>
-						<Carousel.Item>
-							<video
-								width="72%"
-								controls
-								autoPlay
-								type="video/mp4"
-								style={{
-									borderTopRightRadius: "48px",
-									borderTopLeftRadius: "48px",
-									borderBottomRightRadius: "20px",
-									borderBottomLeftRadius: "20px",
-								}}
-							>
-								<source src="videos/telofaz5.mp4" />
-							</video>
-						</Carousel.Item>
-						<Carousel.Item>
-							<video
-								width="72%"
-								controls
-								autoPlay
-								type="video/mp4"
-								style={{
-									borderTopRightRadius: "48px",
-									borderTopLeftRadius: "48px",
-									borderBottomRightRadius: "20px",
-									borderBottomLeftRadius: "20px",
-								}}
-							>
-								<source src="videos/sonsahne.mp4" />
+								<source src="videos/mitoz6.mp4" />
 							</video>
 						</Carousel.Item>
 					</Carousel>
