1b80e30429d167c67555b9f69f72a425fd50c660
Melih Karakaya <46146749+Thynic@users.noreply.github.com>
2024-01-07 16:52:25 +0300

    yazı boyutları, test eklenmesi

--- src/components/Firstpage.js
+++ src/components/Firstpage.js
@@ -1,39 +1,42 @@
-import React from 'react';
-import { CVlogo, Huclogo } from '../icon';
-import '../styles/firstpage.css';
-import Secondpage from './Secondpage';
+import React from "react";
+import { CVlogo, Huclogo, TestIcon } from "../icon";
+import "../styles/firstpage.css";
+import Secondpage from "./Secondpage";
 
 const handleClickScroll = () => {
-	const element = document.getElementById('section-1');
+	const element = document.getElementById("section-1");
 	if (element) {
-		element.scrollIntoView({ behavior: 'smooth' });
+		element.scrollIntoView({ behavior: "smooth" });
 	}
 };
 
 function Firstpage() {
 	return (
-		<div className="fpsize">
-			<div className="fp-container">
-				<div className="fp-isim">
+		<div className='fpsize'>
+			<div className='fp-container'>
+				<div className='fp-isim'>
 					Matematik ve Fen Bilimleri Eğitimi Anabilim Dalı / Fen Eğitimi Bilim
 					Dalı / Doktora Tez Çalışması
 				</div>
-				<div className="fp-icons">
-					<div className="hclogo" onClick={handleClickScroll}>
+				<div className='fp-icons'>
+					<div className='hclogo' onClick={handleClickScroll}>
 						<Huclogo />
 					</div>
-					<a className="cvlogo" href="CV.pdf" target="_blank">
+					<a className='cvlogo' href='CV.pdf' target='_blank'>
 						<CVlogo />
 					</a>
+					<a className='cvlogo test' href='test.pdf' target='_blank'>
+						<TestIcon />
+					</a>
 				</div>
-				<div className="fp-fakulte">İBRAHİM ENES TOSUN</div>
-				<div className="fp-yazi">
+				<div className='fp-fakulte'>İBRAHİM ENES TOSUN</div>
+				<div className='fp-yazi'>
 					Hücre Ünitesi için Geliştirilen WEB 2.0 Aracının Öğrencilerin Akademik
 					Başarılarına, Görsel Alıgılarına ve Sistemsel Düşünme Becerilerine
 					Etkisi
 				</div>
 			</div>
-			<div id="section-1">
+			<div id='section-1'>
 				<Secondpage />
 			</div>
 		</div>
--- src/icon.js
+++ src/icon.js
@@ -1,27 +1,31 @@
 const Tubitaklogo = () => {
-  return <img src="img/tubitaklogo.png" alt="" className="tubitaklogostyle" />;
+	return <img src='img/tubitaklogo.png' alt='' className='tubitaklogostyle' />;
 };
 const Huclogo = () => {
-  return <img src="img/huclogo.png" alt="" className="huclogostyle" />;
+	return <img src='img/huclogo.png' alt='' className='huclogostyle' />;
 };
 const CVlogo = () => {
-  return <img src="img/cvlogo.png" alt="" className="cvlogostyle" />;
+	return <img src='img/cvlogo.png' alt='' className='cvlogostyle' />;
 };
 const Mercek = () => {
-  return <img src="img/mercek.png" alt="" className="mercekstyle" />;
+	return <img src='img/mercek.png' alt='' className='mercekstyle' />;
 };
 const Plantcellmodel = () => {
-  return <img src="img/plant.png" alt="" className="plantstyle" />;
+	return <img src='img/plant.png' alt='' className='plantstyle' />;
 };
 const Animalcellmodel = () => {
-  return <img src="img/animal.png" alt="" className="animalstyle" />;
+	return <img src='img/animal.png' alt='' className='animalstyle' />;
+};
+const TestIcon = () => {
+	return <img src='img/testIcon.png' alt='' className='teststyle' />;
 };
 
 export {
-  Tubitaklogo,
-  Huclogo,
-  CVlogo,
-  Mercek,
-  Animalcellmodel,
-  Plantcellmodel,
+	Tubitaklogo,
+	Huclogo,
+	CVlogo,
+	Mercek,
+	Animalcellmodel,
+	Plantcellmodel,
+	TestIcon,
 };
